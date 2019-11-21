e941eee2c8914807ae7de116296baa315d287986
@slovacus <slovacus@gmail.com>
2019-11-20 21:44:51 -0500

    (#11) chore: implement function for paths

--- src/helpers/tools.zsh
+++ src/helpers/tools.zsh
@@ -1,6 +1,20 @@
 #!/usr/bin/env ksh
 
 function get_path {
-    echo -n "${PATH}" |  \
-        awk -v RS=: '!($0 in a) {a[$0]; printf("%s%s", length(a) > 1 ? ":" : "", $0)}'
+    path::remove_duplicate
+}
+
+function path::remove_duplicate {
+    # Remove duplicate entries from PATH:
+    echo -n "${PATH}" | awk -v RS=':' -v ORS=":" '!a[$1]++{if (NR > 1) printf ORS; printf $a[$1]}'
+}
+
+function path::append {
+    [ -e "${1}" ] && export PATH="${PATH}:$1"
+    PATH="$(path::remove_duplicate)"
+}
+
+function path::prepend {
+    [ -e "${1}" ] && export PATH="${1}:${PATH}"
+    PATH="$(path::remove_duplicate)"
 }
