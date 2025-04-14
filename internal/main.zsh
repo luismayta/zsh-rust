bdea213c9e9280b76099105bf0f42500bc2e11a4
Luis Mayta <luis@hadenlabs.com>
2025-04-14 06:00:16 -0300

    feat ✨(core): implement packages zsh configuration (#698)
    Signed-off-by: Luis Mayta <luis@hadenlabs.com>

--- internal/main.zsh
+++ internal/main.zsh
@@ -4,6 +4,9 @@
 function rust::internal::main::factory {
     # shellcheck source=/dev/null
     source "${ZSH_RUST_PATH}"/internal/base.zsh
+    # shellcheck source=/dev/null
+    source "${ZSH_RUST_PATH}"/internal/packages.zsh
+
     case "${OSTYPE}" in
     darwin*)
         # shellcheck source=/dev/null
@@ -22,3 +25,4 @@ rust::internal::rust::load
 
 if ! core::exists curl; then core::install curl; fi
 if ! core::exists rustc; then rust::internal::rust::install; fi
+if ! core::exists delta; then rust::internal::delta::load; fi
---
+++ internal/packages.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function rust::internal::delta::load {
+  git config --global core.pager delta
+  git config --global interactive.diffFilter 'delta --color-only'
+  git config --global delta.navigate true
+  git config --global merge.conflictStyle zdiff3
+}
